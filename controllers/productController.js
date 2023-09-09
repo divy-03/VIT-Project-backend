2f227da0f4e617b2d00176a6232d9fd6e9bb738c
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-09-09 13:22:54 +0530

    added some tools

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,37 @@
 const Product = require("../models/productModel");
+const resError = require("../tools/resError");
+const resSuccess = require("../tools/resSuccess");
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route working fine" });
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
 };
 
 exports.createProduct = async (req, res) => {
   const product = await Product.create(req.body);
   res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return resError(404, "Product not found", res);
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
     success: true,
     product,
   });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
